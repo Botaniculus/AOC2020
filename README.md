5f2bbe0262326b02e67bd9658482fc8f0c5aabff
Botaniculus <bofin@skaut.cz>
2020-12-23 11:22:07 +0100

    fixed inputs

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AOC2020
-I created the inputs directory recently but didn't change the code (too lazy). For this to work you have to replace "inputX" with "inputs/inputX or fix it in Reader.java."
+
--- Reader.java
+++ Reader.java
@@ -4,7 +4,7 @@ class Reader{
     public static String[] read(String filename){
         ArrayList<String> lines = new ArrayList<String>();
         try{
-            BufferedReader br = new BufferedReader(new FileReader(filename));
+            BufferedReader br = new BufferedReader(new FileReader("inputs/"+filename));
             String line;
             while((line = br.readLine())!=null)
                 lines.add(line);
