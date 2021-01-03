0143376b80389a52d14506c4b07725fcc21eea01
Botaniculus <bofin@skaut.cz>
2021-01-03 16:42:04 +0100

    repaired resource leak with "try with resources" block

--- Reader.java
+++ Reader.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 class Reader{
     public static String[] read(String filename){
         ArrayList<String> lines = new ArrayList<String>();
-        try{
-            BufferedReader br = new BufferedReader(new FileReader("inputs/"+filename));
+        try(BufferedReader br = new BufferedReader(new FileReader("inputs/"+filename));)
+        {
             String line;
             while((line = br.readLine())!=null)
                 lines.add(line);
