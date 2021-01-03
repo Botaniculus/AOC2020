dbc2a49be79b739386d57b0e34aebc8cfd189e57
Botaniculus <bofin@skaut.cz>
2021-01-03 16:17:56 +0100

    repaired

--- Puzzle2.java
+++ Puzzle2.java
@@ -1,36 +1,43 @@
 import java.io.*;
 class Puzzle2{
-	
+
 	public static void main(String[] args){
 		String[] input = Reader.read("input2");
-		int validPasswords = 0;
+		int[] result = solve(input);
+		System.out.println("Part 1: " + result[0]);
+		System.out.println("Part 2: " + result[1]);
+	}
+
+	public static int[] solve(String[] input){
+		int validPasswords1 = 0;
+		int validPasswords2 = 0;
 		for(int i=0; i<input.length; i++){
 				String[] spaceSplitted=input[i].split(" ");
-				String[] num = spaceSplitted[0].split("-");
-				
-				//convert String array to integer array
-				int[] countInt = new int[num.length];
-				for(int f=0; f<num.length; f++){
-					countInt[f]=Integer.parseInt(num[f]);
-				}
-				
-				String character = spaceSplitted[1].replace(":", "");
-				String password = spaceSplitted[2];
-				
-				int calculatedCount=0;			
-				if(String.valueOf(password.charAt(countInt[0]-1)).equals(character)
-				|| (String.valueOf(password.charAt(countInt[1]-1)).equals(character)))
-					calculatedCount++;
-				//if(String.valueOf(password.charAt(countInt[1]-1)).equals(character))
-					//calculatedCount++;
-				
-				if(calculatedCount==1)
-					validPasswords++;
-				
-				System.out.println(input[i] + " - " + calculatedCount + " " + validPasswords);
-		}
-		System.out.println(validPasswords);
-	}
-	
-}
 
+				int min = Integer.parseInt(spaceSplitted[0].split("-")[0]);
+				int max = Integer.parseInt(spaceSplitted[0].split("-")[1]);
+
+				char character = spaceSplitted[1].charAt(0);
+				String password = spaceSplitted[2];
+
+				if(countChars(password, character) >= min && countChars(password, character) <= max)
+					validPasswords1++;
+
+				if((password.charAt(min-1) == character || password.charAt(max-1) == character) && password.charAt(max-1) != password.charAt(min-1))
+					validPasswords2++;
+
+		}
+		int[] toReturn = {validPasswords1, validPasswords2};
+		return toReturn;
+
+	}
+	public static int countChars(String s, char character){
+		int count = 0;
+		for(char c : s.toCharArray()){
+			if(c == character)
+				count++;
+		}
+		return count;
+	}
+
+}
